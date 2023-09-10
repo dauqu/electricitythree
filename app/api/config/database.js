246b29dfef4279c7da2cc5f70234e824affb1bff
Dauqu <95732637+dauqu@users.noreply.github.com>
2023-09-10 13:32:33 +0530

    Update database.js

--- app/api/config/database.js
+++ app/api/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://flutter:7388139606@cluster0.biu5l.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://mazzad:7388139606@cluster0.mfool.mongodb.net/?retryWrites=true&w=majority");
     console.log("MongoDB connection SUCCESS");
     // require('../data/import');
   } catch (error) {
